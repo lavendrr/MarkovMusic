0a01d8a92c433a94fc0e5f68ae9f7b011b15e1b5
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-10-18 20:56:39 -0500

    + Consonance sorting, scale calculator functions

--- MarkovMusic.py
+++ MarkovMusic.py
@@ -1,5 +1,6 @@
 import math
 from fractions import Fraction
+import numpy as np
 
 
 def gcd(a, b):
@@ -22,19 +23,71 @@ class MusicGen:
     def __init__(self, scale):
         self.scale = scale
 
-    def find_most_consonant(self, current_note):
-        most_consonant_note = 0
-        lowest_sum = 1000
+    def calc_interval(self, note1, note2):
+        frac = simplify_fraction(note1, note2)
+        frac_sum = frac[0] + frac[1]
+        return frac_sum
 
-        for n in self.scale:
-            if n != current_note:
-                frac = simplify_fraction(current_note, n)
-                frac_sum = frac[0] + frac[1]
-                if frac_sum < lowest_sum:
-                    lowest_sum = frac_sum
-                    most_consonant_note = n
+    def consonant_sort(self, current_note):
+        sort = []
+        sort = sorted(self.scale, key=lambda x: self.calc_interval(current_note, x))
 
-        return most_consonant_note
+        return sort
+
+    def markov():
+        # matrix = np.matrix(np.zeros(shape = (8, 8)))
+        # matrix = np.array([[1, 2], [3, 4], [5, 6]])
+        matrix = np.array(np.zeros(shape=(8, 8)))
+        probabilities = []
+        x = 0
+        while x < 8:
+            probabilities.append(0.2 - ((3 / 140) * x))
+            x += 1
+
+        x = 0
+
+        for val in matrix.T[0]:
+            # print(val)
+            # matrix.T[int(val)] = x
+            # x += 1
+            matrix.T[0][x] = probabilities[x]
+            x += 1
+
+        return matrix
+
+
+def _12tet(root):
+    scale = []
+    semitone = 0
+    while semitone < 13:
+        print(semitone)
+        scale.append((2 ** (semitone / 12)) * root)
+        semitone += 1
+    return scale
+
+
+def _just(root):
+    scale = []
+    ratios = [
+        1 / 1,
+        12 / 11,
+        9 / 8,
+        6 / 5,
+        5 / 4,
+        4 / 3,
+        3 / 2,
+        8 / 5,
+        5 / 3,
+        7 / 4,
+        11 / 6,
+        2 / 1,
+    ]
+    semitone = 0
+    while semitone < 12:
+        print(semitone)
+        scale.append(root * ratios[semitone])
+        semitone += 1
+    return scale
 
 
 c_just_scale = [261, 293.625, 326.25, 348.0, 391.5, 435.0, 489.375, 522.0]
