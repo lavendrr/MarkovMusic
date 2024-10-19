534828f754a5167a135c8e2bb4dcb29e67be5a1c
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-10-18 21:19:45 -0500

    + Markov matrix sorting for 1 column

--- MarkovMusic.py
+++ MarkovMusic.py
@@ -2,7 +2,6 @@ import math
 from fractions import Fraction
 import numpy as np
 
-
 def gcd(a, b):
     if a < b:
         return gcd(b, a)
@@ -21,7 +20,9 @@ def simplify_fraction(num, denom):
 
 class MusicGen:
     def __init__(self, scale):
-        self.scale = scale
+        self.scale = []
+        for index, item in enumerate(scale):
+            self.scale.append((index, item))
 
     def calc_interval(self, note1, note2):
         frac = simplify_fraction(note1, note2)
@@ -30,13 +31,11 @@ class MusicGen:
 
     def consonant_sort(self, current_note):
         sort = []
-        sort = sorted(self.scale, key=lambda x: self.calc_interval(current_note, x))
+        sort = sorted(self.scale, key=lambda x: self.calc_interval(current_note, x[1]))
 
         return sort
 
-    def markov():
-        # matrix = np.matrix(np.zeros(shape = (8, 8)))
-        # matrix = np.array([[1, 2], [3, 4], [5, 6]])
+    def markov(self, sorted_scale):
         matrix = np.array(np.zeros(shape=(8, 8)))
         probabilities = []
         x = 0
@@ -44,14 +43,10 @@ class MusicGen:
             probabilities.append(0.2 - ((3 / 140) * x))
             x += 1
 
-        x = 0
-
-        for val in matrix.T[0]:
-            # print(val)
-            # matrix.T[int(val)] = x
-            # x += 1
-            matrix.T[0][x] = probabilities[x]
-            x += 1
+        for index, note in enumerate(sorted_scale):
+            # print(f"note index {note[0]}\nnote value {note[1]}")
+            # print(f"probability {probabilities[index]}")
+            matrix.T[0][note[0]] = probabilities[index]
 
         return matrix
 
@@ -91,3 +86,8 @@ def _just(root):
 
 
 c_just_scale = [261, 293.625, 326.25, 348.0, 391.5, 435.0, 489.375, 522.0]
+
+mus = MusicGen(c_just_scale)
+s = mus.consonant_sort(261)
+m = mus.markov(s)
+print(m)
